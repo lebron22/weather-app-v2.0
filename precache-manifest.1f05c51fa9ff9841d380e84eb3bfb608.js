86c59988ead58d9f95323eeda6c7632779252869
Kamil Bronisz <kbronisz8@gmail.com>
2019-09-05 16:25:17 +0200

    Updates

---
+++ precache-manifest.1f05c51fa9ff9841d380e84eb3bfb608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97e912f8cc7eadfdb2131aa599e66dc",
+    "url": "/weather-app-v2.0/index.html"
+  },
+  {
+    "revision": "48c8f7445cb159e77c96",
+    "url": "/weather-app-v2.0/static/js/2.6d791acd.chunk.js"
+  },
+  {
+    "revision": "65c2bb069a69d14f2078",
+    "url": "/weather-app-v2.0/static/js/main.32b41ec5.chunk.js"
+  },
+  {
+    "revision": "a2a2f35027f5cc2d5a68",
+    "url": "/weather-app-v2.0/static/js/runtime~main.09bc2750.js"
+  },
+  {
+    "revision": "4e72ab63e8e13a85d3c837c842bc3b7d",
+    "url": "/weather-app-v2.0/static/media/fog.4e72ab63.jpg"
+  },
+  {
+    "revision": "be21cf71d90698cd81e19eb78d4d3275",
+    "url": "/weather-app-v2.0/static/media/sadsun.be21cf71.png"
+  }
+]);
